ae28b00172215e5b179017a99e5225f5e1253905
Miguelángel <miguelangelcg25@gmail.com>
2015-02-25 23:28:23 -0430

    File reader and writer examples

---
+++ src/nio/FileWriterReaderExample.java
@@ -0,0 +1,51 @@
+package nio;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Migue
+ */
+public class FileWriterReaderExample {
+    
+    public static void main(String[] args) {
+        try {
+            File file = new File("NewFile.txt");    //Just an object, doesn't create the file in disk
+            boolean newFile;
+            
+            System.out.println("File exists? " + file.exists());
+            newFile = file.createNewFile();
+            System.out.println("Created new File? " + newFile);
+            System.out.println("File exists? " + file.exists());
+            //1st time run -> false true true
+            //2nd time run -> true false true   
+        } catch (IOException ex) {
+            Logger.getLogger(FileWriterReaderExample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //write and read a file
+        System.out.println();
+        File anotherFile = new File("TextFile.txt");
+
+        try (FileWriter out = new FileWriter(anotherFile);
+             FileReader in = new FileReader(anotherFile)) {
+            out.write("howdy\nfolks!\n");
+            out.flush();        
+            
+            char buffer[] = new char[50];
+            int size = in.read(buffer);            
+            System.out.println(size + " characters readed");
+            
+            for (char c : buffer) {
+                System.out.print(c);
+            }            
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }        
+    }    
+}
